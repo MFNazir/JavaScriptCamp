b6d5e81bf467d6d3cb473b51a2505e16f699ec57
mfnazir <mnazir9876@gmail.com>
2020-01-09 19:15:07 -0600

    Working directory

---
+++ Section 2/script.js
@@ -0,0 +1,42 @@
+//Basic Variable declarations and data types 
+/*
+var FirstName = 'John'; //String
+console.log(FirstName);
+
+var LastName = 'Smith'; 
+var age = 18; //Numerical 
+
+var fullAge = true; //Boolean
+console.log(fullAge);
+
+var job; 
+console.log(job);
+
+job = 'Teacher';
+console.log(job);
+*/
+
+// Variable Mutations
+/*
+var FirstName = 'John'; 
+var age = 28;
+
+console.log(FirstName + ' ' +  age)
+
+var job, isMarried;
+job = 'Teacher'; 
+isMarried = false;
+
+console.log(FirstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried );
+
+age = 'Twenty-Eight';
+job = 'Driver';
+alert(FirstName + ' is a ' + age + ' years old ' + job + '. Is he married? ' + isMarried );
+
+var LastName = prompt('What is his last name?');
+console.log(FirstName + ' ' + LastName);
+
+*/
+
+
+
