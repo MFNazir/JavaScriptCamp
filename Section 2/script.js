5e5c138feef912ff3abe365c7681917f0b52198d
mfnazir <mnazir9876@gmail.com>
2020-01-11 00:16:54 -0600

    Upto Video 23

--- Section 2/script.js
+++ Section 2/script.js
@@ -314,3 +314,66 @@ yearsUntilRetirement(1990, 'John');
 yearsUntilRetirement(1948, 'Mike');
 yearsUntilRetirement(1969, 'Jane');
 */
+
+/*****************************
+* Function Statements and Expressions
+*/
+/*
+// Function declaration
+//function whatDoYouDo(job, firstName) {}
+
+// Function expression
+var whatDoYouDo = function(job, firstName) {
+    switch(job) {
+        case 'teacher':
+            return firstName + ' teaches kids how to code';
+        case 'driver':
+            return firstName + ' drives a cab in Lisbon.'
+        case 'designer':
+            return firstName + ' designs beautiful websites';
+        default:
+            return firstName + ' does something else';
+    }
+}
+
+console.log(whatDoYouDo('teacher', 'John'));
+console.log(whatDoYouDo('designer', 'Jane'));
+console.log(whatDoYouDo('retired', 'Mark')); 
+*/
+
+/*****************************
+* Arrays
+*/
+/*
+// Initialize new array
+var names = ['John', 'Mark', 'Jane'];
+var years = new Array(1990, 1969, 1948);
+
+
+console.log(names[1]);
+console.log(names.length);
+
+// Mutate array data
+names[1] = 'Ben';
+names[names.length] = 'Mary'; //Adds Mary to the last arrary position 
+console.log(names);
+
+// Different data types
+var john = ['John', 'Smith', 1990, 'designer', false];
+
+john.push('red');
+john.unshift('Mr.');
+console.log(john);
+
+john.pop(); //loses last one 
+john.shift(); //loses first one
+console.log(john);
+
+
+
+console.log(john.indexOf(23)); //returns -1 because nothing is there in array that matches that
+var isDesigner = john.indexOf('designer') === -1 ? 'John is NOT a designer' : 'John IS a designer';
+console.log(isDesigner);
+*/
+
+
